af3e5c7118dabac19661050c2f7a3acc8bd31c48
karlo <karlojacmenjak@gmail.com>
2022-08-01 15:48:18 +0200

    Git List Function

--- util/git.lua
+++ util/git.lua
@@ -1,27 +1,37 @@
-args = {...}
 --https://docs.github.com/en/rest/repos/contents
+ccrings = require("cc.strings")
+args = {...}
 
+git_api = {link = function (repo,folder) return string.format("https://api.github.com/repos/%s/contents/%s",repo, folder) end}
 git_url = "https://raw.githubusercontent.com/"
-repository = "zpqrtbnk/test-repo/"
+repository = "zpqrtbnk/test-repo"
 
 local function setup()
-    --print(textutils.serialize(settings.getNames()))
+    for i=0,255,1 do
+        print(i,string.char(i))
+        sleep(0.3)
+    end
 end
 
 function version()
     print("Git version 0.0.1")
 end
 
-function list()
-    get_response = http.get("https://api.github.com/repos/zpqrtbnk/test-repo/contents")
-    for k, v in pairs(get_response) do
-        print(k,v)
+function list(repo, folder)
+    repo = repo or repository
+    folder = folder or ""
+    get_response = http.get(git_api["link"](repo,folder))
+    repo_files = textutils.unserializeJSON(get_response["readAll"]())
+    for k, v in pairs(repo_files) do
+        text_table = {string.char(28).." "..v["name"]}
+        for i=2, term.getSize()-string.len(text_table[1])-string.len(v["type"]), 1 do
+            text_table[i] = " "
+        end
+        table.insert(text_table, v["type"])
+        text =  table.concat(text_table)
+        print(text)
+        sleep(math.log(#repo_files))
     end
-    print(textutils.serialize(textutils.unserializeJSON(get_response["readAll"]())))
-    --print(textutils.serialize(get_response["readAll"]())) 
-    local file = fs.open("out.txt", "w")
-    file.write(get_response["readAll"]())
-    file.close()
     get_response["close"]()
 end
 
@@ -37,7 +47,7 @@ function gcall(command, parameter)
     end
 end
 
-setup()
+--setup()
 
 for key, value in pairs(args) do
     if value ~= nil then
